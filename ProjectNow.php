1537c4364ad480f1fed16cd18df75a51cc09f965
Victor Emanuel <victoremanuelsb@gmail.com>
2017-12-09 12:19:10 +0000

    Correções explanation

--- ProjectNow.php
+++ ProjectNow.php
@@ -134,17 +134,22 @@
 <section id="about">
 	<br><br><br>
 	<div class="container text-center col-lg-10 col-lg-offset-1 col-md-13 col-md-offset-5 col-md-6">
-		<table style="width:80%" align="center" class="table">
 		<tbody>
 			<tr>
-				<th><img src="img/esquema.png" alt="Esquema" class="img-responsive" /></div></th>
-				<th>Explicação</div></th>
-				A schematic of a mini hydropower plant is shown in the figure on the side. Where Upstream is the highest part of the river and Downstream is the lowest, with Gross head being the difference of the height between these two heights and Losses in head is the losses with floods and hydraulic circuit in relation to the gross height. Machine house is where you find the turbine, generator and transformer. For the design of the small hydraulic power station it is necessary to indicate the type of turbine to be used, the efficiency of the generator and transformer and the indication of other electrical losses. This project tool implements the complete method of calc, obtaining nominal power and energy produced annually for the proposed hydraulic utilization. These data are based on the river flow curve.
-
-				In addition, it is possible to enter initial investment data, the price of the energy to be sold, for an economic analysis of the investment. Taking the VAL as the residual value at the end of the useful life of the mini hydropower plant, and the IRR, which is the internal rate of return, the highest possible value for the discount rate that would lead to the investment unfeasible.
+				<table style="width:80%" align="center" class="table">
+				  <div align="center">
+				  	<img src="img/esquema.png" alt="Esquema" class="img-responsive" /> </tr>
+				  </th>
+				  <tr>
+				    <td class="tg-yw4l"> <h4> Explanation </tr>
+				  </tr>	
+				  <tr>
+				    <td class="tg-yw4l">A schematic of a mini hydropower plant is shown in the figure on the side. Where Upstream is the highest part of the river and Downstream is the lowest, with Gross head being the difference of the height between these two heights and Losses in head is the losses with floods and hydraulic circuit in relation to the gross height. Machine house is where you find the turbine, generator and transformer. For the design of the small hydraulic power station it is necessary to indicate the type of turbine to be used, the efficiency of the generator and transformer and the indication of other electrical losses. This project tool implements the complete method of calc, obtaining nominal power and energy produced annually for the proposed hydraulic utilization. These data are based on the river flow curve.<br><br>
+				    In addition, it is possible to enter initial investment data, the price of the energy to be sold, for an economic analysis of the investment. Taking the VAL as the residual value at the end of the useful life of the mini hydropower plant, and the IRR, which is the internal rate of return, the highest possible value for the discount rate that would lead to the investment unfeasible.<br><br><br></td>
+				  </tr>
+				</table>
 			</tr>
 		</tbody>
-		</table>
 	</div>
 	<div class="container text-center">
 		
